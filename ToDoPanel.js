f8ebda77f64972e03619796acc8b3a0572aefdfc
JcobV01 <jakubwadycki@gmail.com>
2023-04-13 20:39:32 +0200

    Błędy w rozwijaniu zadań

--- ToDoPanel.js
+++ ToDoPanel.js
@@ -66,7 +66,47 @@ submitBtn.addEventListener('click', ()=>{
             document.getElementById('ListOfSteps'+i).appendChild(ListOfStepsLi);
         }
     }
+
+    //tworzenie menu edycji zadania
+
+    let editOptionsContainer = document.createElement('div');
+    let editOptionImportant = document.createElement('p');
+    let editOptionReallyImportant = document.createElement('p');
+    let editOptionEdition = document.createElement('p');
+    let editOptionRemove = document.createElement('p');
+
+    editOptionsContainer.classList.add('editOptionsContainer');
+    editOptionsContainer.id = 'editOptionsContainer'+i;
+    editOptionsContainer.style.display = 'none'
+
+    document.getElementById('task'+i).appendChild(editOptionsContainer);
+
+    editOptionImportant.classList.add('editOptionImportant');
+    editOptionImportant.id = 'editOptionImportant'+i;
+    editOptionImportant.innerHTML = '<i class="fa-solid fa-exclamation"></i> Ustaw jako ważne';
+
+    document.getElementById('editOptionsContainer'+i).appendChild(editOptionImportant);
+
+    editOptionReallyImportant.classList.add('editOptionReallyImportant');
+    editOptionReallyImportant.id = 'editOptionReallyImportant'+i;
+    editOptionReallyImportant.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> Ustaw jako bardzo ważne';
+
+    document.getElementById('editOptionsContainer'+i).appendChild(editOptionReallyImportant);
+
+    editOptionEdition.classList.add('editOptionEdition');
+    editOptionEdition.id = 'editOptionEdition'+i;
+    editOptionEdition.innerHTML = '<i class="fa-solid fa-pen-to-square"></i> Edytuj';
+
+    document.getElementById('editOptionsContainer'+i).appendChild(editOptionEdition);
+
+    editOptionRemove.classList.add('editOptionRemove');
+    editOptionRemove.id = 'editOptionRemove'+i;
+    editOptionRemove.innerHTML = '<i class="fa-solid fa-trash"></i> Usuń';
+
+    document.getElementById('editOptionsContainer'+i).appendChild(editOptionRemove);
     
+    
+    //tworzenie menu opcji w każdym nowym zadaniu
     
     let optionsContainer = document.createElement('div');
     let showOption = document.createElement('div');
@@ -88,6 +128,8 @@ submitBtn.addEventListener('click', ()=>{
     doneOption.classList.add('taskOptions');
 
     showOption.setAttribute('onclick', 'showMore(event)');
+    doneOption.setAttribute('onclick', 'moveToDone(event)');
+    moreOption.setAttribute('onclick', 'moreOptions(event)');
 
     showOption.innerHTML = `<i class="fa-solid fa-eye" id="IkonOfShow${i}"></i>`
     moreOption.innerHTML = `<i class="fa-solid fa-sliders" id="IkonOfMore${i}"></i>`
@@ -97,19 +139,64 @@ submitBtn.addEventListener('click', ()=>{
     document.getElementById('optionsContainer'+i).appendChild(showOption);
     document.getElementById('optionsContainer'+i).appendChild(moreOption);
     document.getElementById('optionsContainer'+i).appendChild(doneOption);
+
+
+    document.querySelector('#task'+i).style.height = document.querySelector('#task'+i).clientHeight + "px";
     i++;
 })
 
 function showMore(event){
     let actualClickTask = event.target.id.slice(10);
+    let listSize = 0;
+
 
     if(document.querySelector('#TaskDescribe'+ actualClickTask).style.display != 'block'){
         document.querySelector('#TaskDescribe'+ actualClickTask).style.display = 'block';
         document.querySelector('#ListOfSteps'+ actualClickTask).style.display = 'block';
+
+        
+
+        if(document.querySelector('#ListOfSteps'+actualClickTask).innerHTML != ''){
+            listSize = parseInt(document.querySelector('#ListOfSteps'+actualClickTask).clientHeight);
+        }
+        else{
+            listSize = 0;
+        }
+        
+        document.querySelector('#task'+actualClickTask).style.height = (document.querySelector('#task'+actualClickTask).clientHeight + (parseInt(document.querySelector('#TaskDescribe'+actualClickTask).clientHeight) + 19 + listSize + 16)) + "px";
     }
     else{
+        document.querySelector('#task'+actualClickTask).style.height = (document.querySelector('#task'+actualClickTask).clientHeight - (parseInt(document.querySelector('#TaskDescribe'+actualClickTask).clientHeight) + 19 + listSize + 16)) + "px";
+
         document.querySelector('#TaskDescribe'+ actualClickTask).style.display = 'none';
         document.querySelector('#ListOfSteps'+ actualClickTask).style.display = 'none';
-    }
+        
+    }  
+}
+
+function moveToDone(event){
+    let actualClickTask = event.target.id.slice(10);
+    let actualTaskHTML = document.querySelector(`#task`+actualClickTask).innerHTML;
+
+    activeTask.removeChild(document.getElementById('task'+actualClickTask));
+
+    let newDoneTask = document.createElement('div');
+    newDoneTask.id = 'task'+actualClickTask;
+    newDoneTask.classList.add('task');
+    newDoneTask.innerHTML = actualTaskHTML;
+    doneTask.appendChild(newDoneTask);
+}
+
+function moreOptions(event){
+    let actualClickTask = event.target.id.slice(10);
     
+    if(document.querySelector('#editOptionsContainer'+ actualClickTask).style.display != 'block'){
+        document.querySelector('#editOptionsContainer'+ actualClickTask).style.display = 'block';
+        document.querySelector('#task'+actualClickTask).style.height = (document.querySelector('#task'+actualClickTask).clientHeight + document.querySelector('#editOptionsContainer'+ actualClickTask).clientHeight) + "px";
+    }
+    else if(document.querySelector('#editOptionsContainer'+ actualClickTask).style.display == 'block'){
+        document.querySelector('#task'+actualClickTask).style.height = (document.querySelector('#task'+actualClickTask).clientHeight - document.querySelector('#editOptionsContainer'+ actualClickTask).clientHeight) + "px";
+        document.querySelector('#editOptionsContainer'+ actualClickTask).style.display = 'none';
+    }
+
 }
